7e085296c5c7fa54490442717d45e4c95d4ccc11
Saulo Costa <saulotarsobc@gmail.com>
2021-12-27 17:17:34 -0300

    a

--- js/app.js
+++ js/app.js
@@ -1,93 +1,83 @@
-/* 10:20:37 - 29/11/21 */
-var data = {
-    'resultado': '',
-    'corrente': '',
-    'operacao': '',
-    'fator': '',
-};
+class Calc {
+    constructor() {
+            this.resultado = 0;
+            this.corrente = "";
+            this.operacao = "";
+            this.fator = "";
+        }
+        /* set */
+    set setCorrente(i) {
+            this.corrente += i;
+        }
+        /* get */
+    get info() {
+        console.log(this);
+    }
 
-var teclas = document.querySelectorAll('.tecla');
-teclas.forEach(tecla => {
+    set backspace(i) {
+        var i = x.substring(0, this.corrente.length - 1);
+        console.log("backspace");
+        this.corrente = i;
+        this.showResultado;
+    }
+    showResultado() {
+        console.log("showResultado");
+        document.getElementById("resultado").innerHTML = this.resultado;
+    }
+}
 
-    tecla.addEventListener('click', function() {
+let calculadora = new Calc();
+calculadora.info;
 
+var teclas = document.querySelectorAll(".tecla");
+teclas.forEach((tecla) => {
+    tecla.addEventListener("click", function() {
         var keyPressed = tecla.id;
 
-        if (keyPressed == 'igual') {
+        if (keyPressed == "igual") {
             /*  */
         }
 
-        if (keyPressed == 'mais') {
+        if (keyPressed == "mais") {
             /*  */
         }
 
-        if (keyPressed == 'clear') {
+        if (keyPressed == "clear") {
             limparTudo();
         }
 
-        if (keyPressed == '1' ||
-            keyPressed == '2' ||
-            keyPressed == '3' ||
-            keyPressed == '4' ||
-            keyPressed == '5' ||
-            keyPressed == '6' ||
-            keyPressed == '7' ||
-            keyPressed == '8' ||
-            keyPressed == '9') {
-            data['corrente'] += tecla.id;
-            showResulto(data['corrente']);
+        if (
+            keyPressed == "1" ||
+            keyPressed == "2" ||
+            keyPressed == "3" ||
+            keyPressed == "4" ||
+            keyPressed == "5" ||
+            keyPressed == "6" ||
+            keyPressed == "7" ||
+            keyPressed == "8" ||
+            keyPressed == "9"
+        ) {
+            /* calculadora.setCorrente = tecla.id;
+                              calculadora.showResultado; */
         }
 
-        if (keyPressed == 'backspace') {
-            backspace();
+        if (keyPressed == "backspace") {
+            /* calculadora.backspace; */
         }
 
-        if (keyPressed == '0') {
+        if (keyPressed == "0") {
             /*  */
         }
 
-        if (keyPressed == 'ponto') {
+        if (keyPressed == "ponto") {
             /*  */
         }
 
         /* debug */
         showVars();
-
-    })
-
+    });
 });
 
-/* showResulto */
-function showResulto(resulto) {
-    document.getElementById('resulto').innerHTML = resulto;
-}
-
-/* limparDisplay */
-function limparDisplay() {
-    document.getElementById('resulto').innerHTML = "";
-}
-
-/* limparTudo */
-function limparTudo() {
-    data = {
-        'resultado': '',
-        'corrente': '',
-        'operacao': '',
-        'fator': '',
-    };
-    limparDisplay();
-}
-
-/* backspace */
-function backspace() {
-    var x = data['corrente'];
-    var i = x.substring(0, x.length - 1);
-    data['corrente'] = i;
-    showResulto(data['corrente']);
-    /*  */
-}
-
-/* showVars */
 function showVars() {
-    console.log(data);
+    calculadora.info;
 }
